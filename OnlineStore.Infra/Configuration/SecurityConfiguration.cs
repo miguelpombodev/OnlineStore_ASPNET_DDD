f178adbeeffd3022241571cf5cfc726d0f1a4e41
Miguel Pombo <miguel.pomboshape@modec.com>
2023-05-05 00:41:18 -0300

    feat: hashing password in database
    feat: generating JWT Token

---
+++ OnlineStore.Infra/Configuration/SecurityConfiguration.cs
@@ -0,0 +1,7 @@
+namespace OnlineStore.Infra.Configuration
+{
+    public static class SecurityConfiguration
+    {
+        public static string JWTKey { get; set; } = "NTEzN2MxMWUtMDkxNy00ODBmLWE2MzctNTdjNzcxMDE4N2Iz";
+    }
+}
--- OnlineStore.Infra/Repositories/CustomerRepository.cs
+++ OnlineStore.Infra/Repositories/CustomerRepository.cs
@@ -1,4 +1,5 @@
 using Microsoft.EntityFrameworkCore;
+using OnlineStore.Domain.DTO;
 using OnlineStore.Domain.Interfaces.Repositories;
 using OnlineStore.Domain.Models;
 using OnlineStore.Infra.Context;
@@ -13,11 +14,54 @@ namespace OnlineStore.Infra.Repositories
             _context = context;
         }
 
+
         async public Task<Customer> GetById(Guid id)
         {
             var user = await _context.Set<Customer>().AsNoTracking().FirstOrDefaultAsync(x => x.Id == id);
 
             return user;
+        }
+
+        async public Task<Customer> GetByEmail(string email)
+        {
+            try
+            {
+                var user = await _context.Set<Customer>().AsNoTracking().FirstOrDefaultAsync(x => x.Email == email);
+
+                return user;
+            }
+            catch (DbUpdateException e)
+            {
+
+                throw new DbUpdateException(e.ToString());
+            }
+        }
+
+        async public Task<Customer> Save(CreateCustomerDTO customer)
+        {
+            try
+            {
+                var encryptedPassword = BCrypt.Net.BCrypt.HashPassword(customer.Password);
+
+                var createUser = new Customer(
+                    customer.Name,
+                    customer.Surname,
+                    customer.CPF,
+                    customer.Email,
+                    customer.Password = encryptedPassword,
+                    DateTime.Now,
+                    DateTime.Now
+                );
+
+                await _context.AddAsync(createUser);
+                await _context.SaveChangesAsync();
+
+                return createUser;
+            }
+            catch (DbUpdateException e)
+            {
+                throw new DbUpdateException(e.ToString());
+            }
         }
     }
 }
